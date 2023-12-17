c64783536ea883cfafa1f943986389699f11df1f
Tanvir A <tanvir@usarus.co.nz>
2023-12-17 23:00:49 +0600

    JS updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
+// Timeline for animations in section 1
 const tl = gsap.timeline();
-
 tl.from(".section-1 :is(h1, p)", {
     duration: 1,
     opacity: 0,
@@ -19,16 +19,16 @@ tl.from(".section-1 :is(h1, p)", {
     repeat: -1,
     y: 10,
     yoyo: true
-})
+});
 
-let tl2 = gsap.timeline({
-    scrollTrigger:{
+// Timeline for animations triggered by scrolling to section 2
+const tl2 = gsap.timeline({
+    scrollTrigger: {
         trigger: ".section-2",
         scroller: "body",
         start: "top 50%"
     }
-})
-
+});
 tl2.from(".section-2 p:nth-child(1)", {
     duration: 1,
     y: 200,
@@ -49,48 +49,46 @@ tl2.from(".section-2 p:nth-child(1)", {
     duration: 1,
     y: 200,
     opacity: 0
-})
+});
 
-let tl3 = gsap.timeline({
-    scrollTrigger:{
+// Timeline for animations triggered by scrolling to section 3
+const tl3 = gsap.timeline({
+    scrollTrigger: {
         trigger: ".section-3",
         scroller: "body",
-        start: "top 50%",
-        markers: true
+        start: "top 50%"
     }
-})
-
+});
 tl3.from(".section-3 .top p", {
     y: 50,
     duration: 1,
     stagger: 0.5,
     opacity: 0
 })
-
-tl3.to(".section-3 .part-1 .line", {
+.to(".section-3 .part-1 .line", {
     height: "30px",
     duration: 1
 })
-tl3.from(".section-3 .part-1 p", {
+.from(".section-3 .part-1 p", {
     opacity: 0,
     duration: 0.5,
     y: 10
 })
-tl3.to(".section-3 .part-2 .line", {
+.to(".section-3 .part-2 .line", {
     height: "90px",
     duration: 1
 })
-tl3.from(".section-3 .part-2 p", {
+.from(".section-3 .part-2 p", {
     opacity: 0,
     duration: 0.5,
     y: 10
 })
-tl3.to(".section-3 .part-3 .line", {
+.to(".section-3 .part-3 .line", {
     height: "420px",
     duration: 1
 })
-tl3.from(".section-3 .part-3 p", {
+.from(".section-3 .part-3 p", {
     opacity: 0,
     duration: 0.5,
     y: 10
-})
+});
